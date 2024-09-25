945cdd60f3c353f178cb656df443ec5eb613f48a
nimishgsk17 <nimishgsk17@gmail.com>
2024-09-25 17:56:15 +0530

    Code added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,98 @@
 package org.example;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        logger.info("Calculator Begins!!");
+        //some changes
+        Scanner reader = new Scanner(System.in);
+        int op, flag=0;
+        double num, exp;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do{
+            System.out.println("\nOperation:");
+            System.out.println("1. Add");
+            System.out.println("2. Subtract");
+            System.out.println("3. Percentile function");
+            System.out.println("4. Muliply");
+            System.out.println("5. Exit\n");
+            System.out.print("Enter your choice(number):");
+            op = reader.nextInt();
+            if(op==5) flag = 1;
+            else{
+                switch(op){
+
+                    case 1:  // addition
+                        System.out.println("\nAdd two numbers!!");
+                        System.out.print("Enter nuber 1: \n");
+                        num = reader.nextInt();
+                        System.out.print("Enter number_ 2: \n");
+                        exp = reader.nextInt();
+                        add(num,exp);
+                        break;
+
+                    case 2: //Subtract
+                        System.out.println("Subtract two numbers!!");
+                        System.out.print("Enter number_ 1: \n");
+                        num = reader.nextInt();
+                        System.out.print("Enter number_ 2: \n");
+                        exp = reader.nextInt();
+                        subt(num,exp);
+                        break;
+
+                    case 3: //Percentile function
+                        System.out.println("Power two numbers!!\n");
+                        System.out.print("Enter number 1: \n");
+                        num = reader.nextInt();
+                        System.out.print("Enter number 2: \n");
+                        exp = reader.nextInt();
+                        per(num,exp);
+                        break;
+
+                    case 4: //Power
+                        System.out.println("Multiply two numbers!!\n");
+                        System.out.print("Enter number1: \n");
+                        num = reader.nextInt();
+                        System.out.print("Enter number2: \n");
+                        exp = reader.nextInt();
+                        mul(num,exp);
+                        break;
+
+                    default:
+                        System.out.println("Exiting due to invalid input!!");
+                        flag = 1;
+                }
+            }
+        }while(flag == 0);
+    }
+
+    public static double add(double num,double exp){
+        double c=num+exp;
+        System.out.println("\nThe Result is "+c+"\n");
+        logger.info("Executing addition for "+num+" and "+exp+". Result: "+c+"\n");
+        return c;
+    }
+
+    public static double subt(double num,double exp){
+        double c=num-exp;
+        System.out.println("\nThe Result is "+c+"\n");
+        logger.info("Executing subtraction for "+num+" and "+exp+". Result: "+c+"\n");
+        return c;
+    }
+    public static double mul(double num,double exp){
+        double c = num*exp;
+        System.out.println("\nThe Result is "+c+"\n");
+        logger.info("Executing multiplication for "+num+" and "+exp+". Result: "+c+"\n");
+        return c;
+    }
+    public static double per(double num,double exp){
+        double c = num%exp;
+        System.out.println("\nThe Result is "+c+"\n");
+        logger.info("Executing percentile operation for "+num+" and "+exp+". Result: "+c+"\n");
+        return c;
     }
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,40 @@
+import org.example.Main;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private Main calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Main();
+    }
+
+    @Test
+    public void test_add() {
+        double a = 3;
+        double b = 7;
+        double expectedResult = 10;
+        double result = calculator.add(a, b);
+        Assert.assertEquals(expectedResult, result, 0.001);
+    }
+
+    @Test
+    public void test_subt() {
+        double a = 10;
+        double b = 4;
+        double expectedResult = 6;
+        double result = calculator.subt(a, b);
+        Assert.assertEquals(expectedResult, result, 0.001);
+    }
+
+    @Test
+    public void test_mul() {
+        double a = 5;
+        double b = 6;
+        double expectedResult = 30;
+        double result = calculator.mul(a, b);
+        Assert.assertEquals(expectedResult, result, 0.001);
+    }
+}
